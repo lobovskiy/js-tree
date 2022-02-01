4c6f31a6ee1c3c8a8d3a3b6969344028a6f06295
Артем Лобовский <alobovskiy@ad.local>
2022-02-01 15:10:19 +0300

    add DOMContentLoaded, cleaning tree

--- src/ctrl/tree.js
+++ src/ctrl/tree.js
@@ -1,15 +1,17 @@
 import { getAsyncTree } from '../api/tree.js';
 import { createBranch } from '../view/tree.js';
 
-const wrapper = document.getElementById('the-tree');
-const button = document.getElementById('load-button');
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.getElementById('the-tree');
+  const button = document.getElementById('load-button');
 
-button.addEventListener('click', loadTree);
+  button.addEventListener('click', loadTree);
 
-function loadTree() {
-  getAsyncTree().then(asyncTreeData => {
-    const tree = createBranch(asyncTreeData);
-
-    wrapper.append(tree);
-  });
-}
+  function loadTree() {
+    getAsyncTree().then(asyncTreeData => {
+      const tree = createBranch(asyncTreeData);
+      wrapper.textContent = '';
+      wrapper.append(tree);
+    });
+  }
+});
