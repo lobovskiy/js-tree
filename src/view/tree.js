caaffa06733eca208a7ad9efc904b5c665f498b3
Артем Лобовский <alobovskiy@ad.local>
2022-02-03 14:52:44 +0300

    refactor js and sass

--- src/view/tree.js
+++ src/view/tree.js
@@ -4,13 +4,55 @@ function createBranch(arr) {
     branch.classList.add('branch');
 
     const nodes = arr.map(createNode);
-
     branch.append(...nodes);
 
-    // const branchWithTails = addTails(branch);
-
-    // return branchWithTails;
     return branch;
+  }
+}
+
+function addName(item, className) {
+  if (item.name) {
+    const nameDiv = document.createElement('div');
+    nameDiv.append(item.name);
+    nameDiv.classList.add(className);
+    return nameDiv;
+  }
+}
+
+function addMarker() {
+  const markerDiv = document.createElement('div');
+  markerDiv.classList.add('branch__node-marker');     // задать класс как переменную
+  return markerDiv;
+}
+
+function getTrails(item) {
+  if (item?.level) {
+    const trails = [];
+
+    for (let i = 1; i <= item.level; i++) {
+      const trailDiv = document.createElement('div');
+      trailDiv.classList.add('branch__node-trail');   // задать класс как переменную
+      addTrailModificator(trailDiv, i);
+      trails.push(trailDiv);
+    }
+
+    return trails;
+  }
+}
+
+function addTrailModificator(trail, level) {
+  trail.classList.add(`branch__node-trail_level${level}`);    // задать класс как переменную
+}
+
+function addTrails(node, trails) {
+  if (Array.isArray(trails)) {
+    trails.forEach(trail => node.append(trail));
+  }
+}
+
+function setNodeLevel(item, node) {
+  if (item?.level) {
+    node.classList.add(`branch__node_level${item.level}`);      // задать класс как переменную
   }
 }
 
@@ -18,84 +60,30 @@ function createNode(item) {
   if (item) {
     const node = document.createElement('li');
     node.classList.add('branch__node');   // задать класс как переменную
-    
-    const divName = document.createElement('div');
-    divName.classList.add('branch__node-name');   // задать класс как переменную
 
-    const divMarker = document.createElement('div');
-    divMarker.classList.add('branch__node-marker');
+    // add trails
+    const trails = getTrails(item);
+    addTrails(node, trails);
 
-    if (item.level == 1) {
-      const divTrailLevel1 = document.createElement('div');
-      divTrailLevel1.classList.add('branch__node-trail', 'branch__node-trail_level1');
-      node.append(divTrailLevel1);
+    // add level class
+    setNodeLevel(item, node);
 
-      node.classList.add('branch__node_level1');
-    }
+    // add marker and name
+    const marker = addMarker();
+    const name = addName(item, 'branch__node-name');      // задать класс как переменную
+    marker && node.append(marker);
+    name && node.append(name);
 
-    if (item.level == 2) {
-      const divTrailLevel1 = document.createElement('div');
-      const divTrailLevel2 = document.createElement('div');
-      divTrailLevel2.classList.add('branch__node-trail', 'branch__node-trail_level2');
-      divTrailLevel1.classList.add('branch__node-trail', 'branch__node-trail_level1');
-      node.append(divTrailLevel1);
-      node.append(divTrailLevel2);
-
-      node.classList.add('branch__node_level2');
-    }
-
-    
-
-    if (item.level == 3) {
-      const divTrailLevel1 = document.createElement('div');
-      const divTrailLevel2 = document.createElement('div');
-      const divTrailLevel3 = document.createElement('div');
-      divTrailLevel3.classList.add('branch__node-trail', 'branch__node-trail_level3');
-      divTrailLevel2.classList.add('branch__node-trail', 'branch__node-trail_level2');
-      divTrailLevel1.classList.add('branch__node-trail', 'branch__node-trail_level1');
-      node.append(divTrailLevel1);
-      node.append(divTrailLevel2);
-      node.append(divTrailLevel3);
-
-      node.classList.add('branch__node_level3');
-    }
-
-    if (item.name) {
-      divName.append(item.name);
-      node.append(divMarker);
-      node.append(divName);
-    }
-
+    // add branch with its class
     const childBranch = createBranch(item.child);
     if (childBranch) {
       node.append(childBranch);
-      divMarker.classList.add('branch__node-marker_expanded');
+      marker.classList.add('branch__node-marker_branch');
     }
     
     return node;
   }
 }
-
-// function addTails(tree) {
-//   if (tree?.childNodes?.length) {
-    
-//     console.dir(tree.childNodes);
-
-//     const addExpandArrows = treeNodes => {
-//       treeNodes.forEach(node => {
-//           const divMarker = document.createElement('div');
-//           divMarker.classList.add('branch__node-marker');     // задать класс как переменную
-//           node.childElementCount > 1 && divMarker.classList.add('branch__node-marker_expanded');
-//           node.prepend(divMarker);
-//       });
-
-//       return treeNodes;
-//     }
-
-//     addExpandArrows(tree.childNodes);
-//     return tree;
-//   }
-// }
 
 function renderTree(tree, container) {
   container.textContent = '';
